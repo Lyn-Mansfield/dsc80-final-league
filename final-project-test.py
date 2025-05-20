2b5ed94eca3349cf5ec8d71f24a310e13297bc29
Lyn Mansfield <lyn@Lyns-MacBook-Pro.local>
2025-05-20 11:13:59 -0700

    analyzed partial row data

--- final-project-test.py
+++ final-project-test.py
@@ -1,22 +1,30 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 from matplotlib.backends.backend_pdf import PdfPages
+from pathlib import Path
 
-df = pd.DataFrame({
-	'A': np.random.uniform(0, 10, 100), 
-	'B': np.random.uniform(0, 10, 100), 
-	'C': np.random.uniform(0, 10, 100)
-	})
+dataset_path = Path("./data/2025_LoL_esports_match_data_from_OraclesElixir.csv")
+# /data/2025_LoL_esports_match_data_from_OraclesElixir.csv
+data_csv = open(dataset_path).read()
+rows = data_csv.split('\n')
+columnss = rows[0].split(',')
+rowss = [x.split(',') for x in rows[1:]]
+data = pd.DataFrame(columns = columnss, data = rowss)
 
 
-pdf_pages = PdfPages('multipage_plots.pdf')
+complete_samples = data.query("datacompleteness == 'complete'")
+partial_samples = data.query("datacompleteness == 'partial'")
 
-for column in df.columns:
-    fig, ax = plt.subplots()
-    ax.hist(df[column])
-    ax.set_title(f'Graph of {column}')
-    pdf_pages.savefig(fig)
-    plt.close(fig)
+team_rows = partial_samples.query("playername == ''")
+team_rows['firstblood'] = team_rows['firstblood'].astype(str)
+team_rows['result'] = team_rows['result'].astype(str)
 
-pdf_pages.close()
+
+w_first_blood = team_rows.query("firstblood == '1'")
+wo_first_blood = team_rows.query("firstblood == '0'")
+
+
+fig = px.histogram(w_first_blood, x='result')
+fig.show()
